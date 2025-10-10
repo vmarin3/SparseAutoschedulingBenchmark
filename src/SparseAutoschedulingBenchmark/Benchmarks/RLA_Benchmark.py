20700a992d10d10a9bb4a3b62af2306665f4ba16
VMaster123 <73140174+VMaster123@users.noreply.github.com>
2025-10-10 15:24:17 -0400

    Updated changes

--- src/SparseAutoschedulingBenchmark/Benchmarks/RLA_Benchmark.py
+++ src/SparseAutoschedulingBenchmark/Benchmarks/RLA_Benchmark.py
@@ -1,14 +1,19 @@
-import numpy as np
-from ..BinsparseFormat import BinsparseFormat
+
 
 """
-Name: Randomized Linear Algebra and Algorithms
+Name: Randomized Numerical Linear Algebra and Algorithms. 
 Author: Vilohith Gokarakonda
 
 The purpose of this is to create python tests that are for Randomized Linear Algebra methods.
     Specifically, I will first show the application of the Johnson Lindenstrauss Lemma for Nearest Neighbor. 
     My goal is to write benchmarks on applications of Randomized Numerical Linear Algebra, paticuarily
-    for graph algorithms in PDEs.
+    for graph algorithms, PDEs, and Scientific Machine Learning.
+
+    Next PR will be from this paper: SPARSE GRAPH BASED SKETCHING FOR FAST NUMERICAL LINEAR ALGEBRA
+
+    My semester goal project will be to understand Learning Green’s functions associated with time-dependent
+    partial differential equations and start writing code in Finchlite an understand finch implementation of these methods. 
+
 """
 
 
@@ -38,12 +43,14 @@ def benchmark_johnson_lindenstrauss_nn(xp, data_bench, query_bench, k=5, eps=0.1
 
     distances = xp.zeros(n_samples)
 
+    # Norm Calculations
     diff = projected_data - projected_query
     distances = xp.sqrt(xp.sum(diff**2, axis = 1))
 
     nearest_indices = xp.zeros(k)
     nearest_distances = xp.full(k, xp.inf)
 
+    # No need for einsum, since I am only iterating over one dimension of the matrix.
     for i in range(n_samples):
         current_dist = distances[i]
         for j in range(k):
@@ -56,6 +63,7 @@ def benchmark_johnson_lindenstrauss_nn(xp, data_bench, query_bench, k=5, eps=0.1
                 nearest_indices[j] = i
                 break
 
+    # Just puts the results in 3 by 5 matrix. nearest_indices is scalar that associates to sample point i in original space. Distance is in projected subspace.  
     result_indices = xp.stack([xp.arange(k), nearest_indices], axis=0)
     result = xp.stack([result_indices, nearest_distances], axis=0)
 
