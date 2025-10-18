44e4ca19ee84276ba6bc174c931add796c78163d
Benjamin Berol <91988767+BenBerol@users.noreply.github.com>
2025-10-17 21:36:11 -0400

    Jacobi Data Generators (#32)
    * Implement framework for realistic data (matrices don't converge :(_
    
    * update matmul syntax
    
    * Fix data bug! And implement data generators
    
    * Clean up data generator code
    
    * Add matrix analysis code
    
    * add scraper
    
    * updates
    
    * incorporate new data generators into jacobi
    
    * update SS matrix scraper
    
    * remove arrays with over 20,000 rows
    
    * move scrape_matrices to utils folder and fix error handling
    
    * Clarify data collection criteria in Jacobi.py
    
    Updated data collection description to specify positive semidefinite matrices.
    
    ---------
    
    Co-authored-by: Willow Ahrens <willow@csail.mit.edu>

--- src/SparseAutoschedulingBenchmark/BenchmarkRunner.py
+++ src/SparseAutoschedulingBenchmark/BenchmarkRunner.py
@@ -1,7 +1,17 @@
 import argparse
 import time
 
-from .Benchmarks.Jacobi import benchmark_jacobi, dg_jacobi_sparse_small
+from .Benchmarks.Jacobi import (
+    benchmark_jacobi,
+    dg_jacobi_sparse_1,
+    dg_jacobi_sparse_2,
+    dg_jacobi_sparse_3,
+    dg_jacobi_sparse_4,
+    dg_jacobi_sparse_5,
+    dg_jacobi_sparse_6,
+    dg_jacobi_sparse_7,
+    dg_jacobi_sparse_8,
+)
 from .Benchmarks.MatMul import (
     benchmark_matmul,
     dg_matmul_dense_large,
@@ -21,7 +31,16 @@ DATA_GENERATOR_DICT = {
         "matmul_sparse_small": dg_matmul_sparse_small,
         "matmul_sparse_large": dg_matmul_sparse_large,
     },
-    "jacobi": {"jacobi_sparse_small": dg_jacobi_sparse_small},
+    "jacobi": {
+        "jacobi_sparse_1": dg_jacobi_sparse_1,
+        "jacobi_sparse_2": dg_jacobi_sparse_2,
+        "jacobi_sparse_3": dg_jacobi_sparse_3,
+        "jacobi_sparse_4": dg_jacobi_sparse_4,
+        "jacobi_sparse_5": dg_jacobi_sparse_5,
+        "jacobi_sparse_6": dg_jacobi_sparse_6,
+        "jacobi_sparse_7": dg_jacobi_sparse_7,
+        "jacobi_sparse_8": dg_jacobi_sparse_8,
+    },
 }
 
 
