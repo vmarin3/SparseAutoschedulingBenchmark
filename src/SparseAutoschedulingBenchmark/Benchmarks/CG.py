9d666687c0cd48cfb1407e85b0456445ed215f48
Ben Berol <benberol40@gmail.com>
2025-10-23 19:54:10 -0400

    prevent buildup of lazy operations

--- src/SparseAutoschedulingBenchmark/Benchmarks/CG.py
+++ src/SparseAutoschedulingBenchmark/Benchmarks/CG.py
@@ -1,3 +1,6 @@
+"""https://arxiv.org/abs/2007.00640 Page 21"""
+
+
 def benchmark_cg(
     xp, A_bench, b_bench, x_bench, rel_tol=1e-8, abs_tol=1e-20, max_iters=10_000
 ):
@@ -5,34 +8,36 @@ def benchmark_cg(
     b = xp.lazy(xp.from_benchmark(b_bench))
     x = xp.lazy(xp.from_benchmark(x_bench))
 
-    tolerance = max(xp.compute(xp.lazy(rel_tol) * norm(xp, b))[()], abs_tol)
+    tolerance = max(
+        xp.compute(xp.lazy(rel_tol) * xp.sqrt(xp.vecdot(b, b)))[()], abs_tol
+    )
+    # tol_sq used to avoid having to sqrt dot products when checking tolerance
     tol_sq = tolerance * tolerance
     max_iters = min(max_iters, xp.shape(A)[0])
 
     r = b - A @ x
     p = r
     it = 0
-    rr = xp.vecdot(r, r)
+    rr = xp.compute(xp.vecdot(r, r))[()]
 
-    while xp.compute(rr)[()] >= tol_sq and it < max_iters:
+    while rr >= tol_sq and it < max_iters:
         Ap = A @ p
-        alpha = rr / xp.vecdot(p, Ap)
+        alpha = rr / xp.vecdot(r, Ap)
         x += alpha * p
         r -= alpha * Ap
 
-        new_rr = xp.vecdot(r, r)
+        new_rr = xp.compute(xp.vecdot(r, r))[()]
 
-        if xp.compute(new_rr)[()] <= tol_sq:
+        if new_rr <= tol_sq:
             break
 
         beta = new_rr / rr
         p = r + beta * p
         rr = new_rr
 
+        x = xp.lazy(xp.compute(x))
+        r = xp.lazy(xp.compute(r))
+        p = xp.lazy(xp.compute(p))
         it += 1
     x_solution = xp.compute(x)
     return xp.to_benchmark(x_solution)
-
-
-def norm(xp, v):
-    return xp.sqrt(xp.sum(xp.multiply(v, v)))
